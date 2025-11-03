b608ab849c18cddb4c82916f24ccaf41518aace7
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-03 12:28:07 +0630

    new py

---
+++ amir2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+amir_deals=pd.read_csv("amir_deals.csv")
+# Set random seed
+np.random.seed(24)
+
+# Sample 5 deals without replacement
+sample_without_replacement = amir_deals.sample(5)
+print(sample_without_replacement)
