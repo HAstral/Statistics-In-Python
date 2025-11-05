7161b3c9ea32e9fc8f883604af8993212b4b3df5
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-05 21:39:44 +0630

    new lines

--- correlation.py
+++ correlation.py
@@ -7,3 +7,8 @@ sns.lmplot(x='life_exp',y='happiness_score',data=world_happiness,ci=None)
 
 # Show plot
 plt.show()
+
+# Correlation between life_exp and happiness_score
+cor = world_happiness['life_exp'].corr(world_happiness['happiness_score'])
+
+print(cor)
